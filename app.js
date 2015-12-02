0530e7924f6886937f4671657080cd9d1588129e
Tom Thorley <tgthorley@gmail.com>
2015-12-02 20:33:22 +0000

    added basic webserver to validate running

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var Bot = require("./bot"),
     Gatherer = require("./gatherer"),
     unirest = require('unirest'),
     Bitly = require('bitly');
+    express = require("express"),
+    morgan = require('morgan'),
+    app = express();
 
 var app_config = require("./app_config"),
     config = require("./config");
@@ -15,3 +18,17 @@ var bot = new Bot(app_config.twitter),
     bitly = new Bitly(app_config.bitly.ACCESS_TOKEN);
 
 sched.schedule(bot);
+
+/*
+* basic Webserver to verify running
+*/
+
+var portapp = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var ipapp = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+
+app.listen(portapp,ipapp, function(){
+  console.log("http sevrer running on " + ipapp + ":" + portapp);
+});
